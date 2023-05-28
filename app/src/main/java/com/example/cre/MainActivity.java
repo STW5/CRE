3dcc58906c53dd7fd5522d538b2b042ae601bea2
Taewoong Seo <xodnd8384@gmail.com>
2023-05-28 18:21:14 +0900

    second

--- app/src/main/java/com/example/cre/MainActivity.java
+++ app/src/main/java/com/example/cre/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.cre;
 
 import android.Manifest;
-import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.pdf.PdfDocument;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.method.ScrollingMovementMethod;
-import android.util.SparseArray;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -24,31 +25,28 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.Text;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
-import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import com.google.mlkit.common.MlKitException;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
+import java.io.OutputStream;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
     private static final int CAMERA_CAPTURE_REQUEST_CODE = 200;
     private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 300;
+
+    private static final int READ_MEDIA_IMAGES_PERMISSION_REQUEST_CODE = 400;
+
 
     private ImageView imageView;
     private Bitmap capturedPhoto;
@@ -87,6 +85,58 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, CAMERA_CAPTURE_REQUEST_CODE);
     }
+
+    public void onSaveToGalleryClick(View view) {
+        if (capturedPhoto != null) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+            Bitmap photo = bitmapDrawable.getBitmap();
+            savePhotoToGallery(photo);
+        } else {
+            Toast.makeText(this, "No photo captured", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void savePhotoToGallery(Bitmap photo) {
+        String imageFileName = "captured_photo.jpg";
+
+        OutputStream fos;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ContentResolver resolver = getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, imageFileName);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM);
+
+            Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+            try {
+                fos = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+                if (fos != null) {
+                    photo.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    fos.close();
+                    Toast.makeText(this, "영수증이 갤러리에 저장되었습니다.", Toast.LENGTH_SHORT).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "저장에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            File imageFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), imageFileName);
+            try {
+                fos = new FileOutputStream(imageFile);
+                photo.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.close();
+
+                // Update gallery
+                MediaScannerConnection.scanFile(this, new String[]{imageFile.getAbsolutePath()}, null, null);
+
+                Toast.makeText(this, "Photo saved to gallery", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Failed to save photo to gallery", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
     public void onConvertToPdfClick(View view) {
         if (capturedPhoto != null) {
@@ -142,54 +192,73 @@ public class MainActivity extends AppCompatActivity {
             if (extras != null) {
                 capturedPhoto = (Bitmap) extras.get("data");
                 imageView.setImageBitmap(capturedPhoto);
-                
+
                 performGoogleVisionAPICall(capturedPhoto);
             }
+        }
+
+        if (requestCode == READ_MEDIA_IMAGES_PERMISSION_REQUEST_CODE && resultCode == RESULT_OK) {
+            // Get the selected image URI
+            Uri imageUri = data.getData();
+
+            // Do something with the selected image URI, such as displaying it in an ImageView
+            imageView.setImageURI(imageUri);
         }
     }
 
     private void performGoogleVisionAPICall(Bitmap photo) {
-        // Configure the API request
-        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
-        Frame frame = new Frame.Builder().setBitmap(photo).build();
+        // Configure the text recognizer
+        KoreanTextRecognizerOptions options = new KoreanTextRecognizerOptions.Builder().build();
+        TextRecognizer recognizer = TextRecognition.getClient(options);
 
-        // Perform the API call
-        SparseArray<TextBlock> textBlocks = textRecognizer.detect(frame);
+        // Create an ML Kit InputImage from the Bitmap
+        InputImage image = InputImage.fromBitmap(photo, 0);
 
-        // Process the API response
-        StringBuilder extractedText = new StringBuilder();
-        for (int i = 0; i < textBlocks.size(); i++) {
-            TextBlock textBlock = textBlocks.valueAt(i);
-            extractedText.append(textBlock.getValue());
-            extractedText.append("\n");
+        // Process the image using the text recognizer
+        recognizer.process(image)
+                .addOnSuccessListener(text -> {
+                    // Process the text recognition result
+                    StringBuilder extractedText = new StringBuilder();
+                    for (Text.TextBlock textBlock : text.getTextBlocks()) {
+                        extractedText.append(textBlock.getText());
+                        extractedText.append("\n");
+                    }
+
+                    extractedTextView.setMovementMethod(new ScrollingMovementMethod());
+                    extractedTextView.setText(extractedText.toString());
+
+                    Toast.makeText(this, "완료", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    // Handle any errors
+                    Toast.makeText(this, "Failed to process image: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
+
+
+    public void onViewPhotosClick(View view) {
+        // Check if the READ_EXTERNAL_STORAGE permission is granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission not granted, request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    READ_MEDIA_IMAGES_PERMISSION_REQUEST_CODE);
+        } else {
+            // Permission granted, show the photos
+            openImageFolder();
         }
-
-        extractedTextView.setMovementMethod(new ScrollingMovementMethod());
-        // Update the TextView with the extracted text
-        extractedTextView.setText(extractedText.toString());
-
-        // Show a toast message with the extracted text
-        Toast.makeText(this, "완료", Toast.LENGTH_SHORT).show();
     }
 
-    public void onOpenPdfButtonClick(View view) {
-        String filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/captured_photo.pdf";
-        openPdfFile(filePath);
-    }
-
-    private void openPdfFile(String filePath) {
-        File file = new File(filePath);
-        Uri fileUri = FileProvider.getUriForFile(this, "com.example.cre.fileprovider", file);
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(fileUri, "application/pdf");
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        try {
+    private void openImageFolder() {
+        Uri imageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        Intent intent = new Intent(Intent.ACTION_VIEW, imageUri);
+        intent.setDataAndType(imageUri, "image/*");
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, "No PDF viewer app found", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "No file manager app found", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -212,6 +281,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "카메라 권한 거부", Toast.LENGTH_SHORT).show();
             }
         }
-
     }
 }
